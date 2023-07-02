5ae22227df0d2f6aef84d590923be3fe19d2cc57
ChowonKim <97011721+chowon986@users.noreply.github.com>
2023-07-02 19:02:12 +0900

    김초원 : 빈 공간 생성(1)

--- Assets/FruitSwipeMatch3Kit-PSDSourcesPack/Scripts/Board/Board.cs
+++ Assets/FruitSwipeMatch3Kit-PSDSourcesPack/Scripts/Board/Board.cs
@@ -10,6 +10,21 @@ public enum GameState
     move
 }
 
+public enum TileKind
+{
+    Breakable,
+    Blank,
+    Normal
+}
+
+[System.Serializable]
+public class TileType
+{
+    public int x;
+    public int y;
+    public TileKind tileKind;
+}
+
 public class Board : MonoBehaviour
 { 
     public int width;
@@ -19,48 +34,64 @@ public class Board : MonoBehaviour
     public GameState currentState = GameState.move;
     public GameObject[] dots;
     public GameObject destroyEffect;
+    public TileType[] boardLayout;
     public GameObject[,] allDots;
     public Dot currentDot;
-    private BackgroundTile[,] allTiles;
+    private bool[,] blankSpace;
     private FindMatches findeMatches;
 
     // Start is called before the first frame update
     void Start()
     {
         findeMatches = FindObjectOfType<FindMatches>();
-        allTiles = new BackgroundTile[width, height];
+        blankSpace = new bool[width, height];
         allDots = new GameObject[width, height];
         Setup();
     }
 
+    public void GenerateBlankSpaces()
+    {
+        for(int i = 0; i < boardLayout.Length; i++)
+        {
+            if (boardLayout[i].tileKind == TileKind.Blank)
+            {
+                blankSpace[boardLayout[i].x, boardLayout[i].y] = true;
+            }
+        }
+    }
+
     private void Setup()
     {
-        for(int i = 0; i < width; i++)
+        GenerateBlankSpaces();
+        for (int i = 0; i < width; i++)
         {
-            for(int j = 0; j < height; j++)
+            for (int j = 0; j < height; j++)
             {
-                Vector2 tempPosition = new Vector2(i, j + offSet);
-                GameObject backgroundTile = Instantiate(tilePrefab, tempPosition, Quaternion.identity) as GameObject;
-                backgroundTile.transform.parent = this.transform;
-                backgroundTile.name = "( " + i + ", " + j + " )";
-                int dotToUse = Random.Range(0, dots.Length);
-                int maxIterations = 0;
-
-                while (MatchesAt(i, j, dots[dotToUse]) &&
-                       maxIterations < 100)
+                if (!blankSpace[i, j])
                 {
-                    dotToUse = Random.Range(0, dots.Length);
-                    maxIterations++;
+                    Vector2 tempPosition = new Vector2(i, j + offSet);
+                    GameObject backgroundTile = Instantiate(tilePrefab, tempPosition, Quaternion.identity) as GameObject;
+                    backgroundTile.transform.parent = this.transform;
+                    backgroundTile.name = "( " + i + ", " + j + " )";
+                    int dotToUse = Random.Range(0, dots.Length);
+                    int maxIterations = 0;
+
+                    while (MatchesAt(i, j, dots[dotToUse]) &&
+                           maxIterations < 100)
+                    {
+                        dotToUse = Random.Range(0, dots.Length);
+                        maxIterations++;
+                    }
+
+                    maxIterations = 0;
+
+                    GameObject dot = Instantiate(dots[dotToUse], tempPosition, Quaternion.identity);
+                    dot.GetComponent<Dot>().row = j;
+                    dot.GetComponent<Dot>().column = i;
+                    dot.transform.parent = this.transform;
+                    dot.name = "( " + i + ", " + j + " )";
+                    allDots[i, j] = dot;
                 }
-
-                maxIterations = 0;
-
-                GameObject dot = Instantiate(dots[dotToUse], tempPosition, Quaternion.identity);
-                dot.GetComponent<Dot>().row = j;
-                dot.GetComponent<Dot>().column = i;
-                dot.transform.parent = this.transform;
-                dot.name = "( " + i + ", " + j + " )";
-                allDots[i, j] = dot;
             }
         }
     }
@@ -69,35 +100,47 @@ public class Board : MonoBehaviour
     {
         if (column > 1 && row > 1)
         {
-            if (allDots[column - 1, row].tag == piece.tag &&
-                allDots[column - 2, row].tag == piece.tag)
+            if (allDots[column - 1, row] != null && allDots[column - 2, row] != null)
             {
-                return true;
+                if (allDots[column - 1, row].tag == piece.tag &&
+                    allDots[column - 2, row].tag == piece.tag)
+                {
+                    return true;
+                }
             }
 
-            if (allDots[column, row - 1].tag == piece.tag &&
-                allDots[column, row - 2].tag == piece.tag)
+            if (allDots[column, row - 1] != null && allDots[column, row - 2] != null)
             {
-                return true;
+                if (allDots[column, row - 1].tag == piece.tag &&
+                allDots[column, row - 2].tag == piece.tag)
+                {
+                    return true;
+                }
             }
         }
         else if (column <= 1 || row <= 1)
         {
             if (row > 1)
             {
-                if (allDots[column, row - 1].tag == piece.tag &&
-                    allDots[column, row - 2].tag == piece.tag)
+                if (allDots[column, row - 1] != null && allDots[column, row - 2] != null)
                 {
-                    return true;
+                    if (allDots[column, row - 1].tag == piece.tag &&
+                    allDots[column, row - 2].tag == piece.tag)
+                    {
+                        return true;
+                    }
                 }
             }
 
             if (column > 1)
             {
-                if (allDots[column - 1, row].tag == piece.tag &&
-                    allDots[column - 2, row].tag == piece.tag)
+                if (allDots[column - 1, row] != null && allDots[column - 2, row] != null)
                 {
-                    return true;
+                    if (allDots[column - 1, row].tag == piece.tag &&
+                    allDots[column - 2, row].tag == piece.tag)
+                    {
+                        return true;
+                    }
                 }
             }
         }
