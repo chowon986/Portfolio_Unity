2b8ace1edea17b2dfb0c515caf1d63475292018a
ChowonKim <97011721+chowon986@users.noreply.github.com>
2023-07-04 15:16:05 +0900

    김초원 : Fade in/out 구현

--- Assets/FruitSwipeMatch3Kit-PSDSourcesPack/Scripts/Button/LevelChangeButton.cs
+++ Assets/FruitSwipeMatch3Kit-PSDSourcesPack/Scripts/Button/LevelChangeButton.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelChangeButton : Button
 {
     public int CurLevel;
+    public Animator panelAnim;
 
     public override void ClickButton()
     {
-        if(tag == "InGame")
-            SceneManager.LoadScene("InGameScene" + CurLevel);
+        if (tag == "InGame")
+        {
+            if(panelAnim != null)
+            {
+                panelAnim.SetBool("isFadeOut", true);
+            }
+        }
         else
             SceneManager.LoadScene("Scene" + (CurLevel + 1));
 
+    }
+
+    private void Update()
+    {
+        AnimatorStateInfo stateInfo = panelAnim.GetCurrentAnimatorStateInfo(0);
+        if(stateInfo.IsName("Panel Fade out"))
+        {
+            if(stateInfo.normalizedTime >= 1.0f)
+            {
+                SceneManager.LoadScene("InGameScene" + CurLevel);
+            }
+        }
     }
 }
